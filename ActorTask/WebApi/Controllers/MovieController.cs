109145101ac6f8181ad4d90eecfdf4f6b95abc59
Astofil <114575163+Astofil@users.noreply.github.com>
2023-01-03 19:07:55 +0300

    first commit

---
+++ ActorTask/WebApi/Controllers/MovieController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Infrastructure.Services;
+using Domain.Wrapper;
+using Domain.Dtos;
+using Domain.Entities;
+using Domain.Filters;
+using Infrastructure.Context;
+using Infrastructure.Seeds;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class MovieController
+{
+    private readonly MovieService _movieService;
+    public MovieController(MovieService movieService)
+    {
+        _movieService = movieService;
+    }
+
+    [HttpGet("GetMoviesByName")]
+    public async Task<PaginationResponse<List<GetMovieDto>>> GetMovieByName([FromQuery]MovieFilter movieName)
+    {
+        return await _movieService.GetMoviesByName(movieName);
+    }
+
+    [HttpGet("GetActors")]
+    public async Task<Response<List<GetActorDto>>> GetActor()
+    {
+        return await _movieService.GetActor();
+    }
+    
+    [HttpGet("GetCategories")]
+    public async Task<Response<List<GetCategoryDto>>> GetCategory()
+    {
+        return await _movieService.GetCategory();
+    }
+}
